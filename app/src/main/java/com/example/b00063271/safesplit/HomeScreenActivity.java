2b8b73dcc157fa7540e0a120103460417721fd83
hussu97 <h_abbasi97@hotmail.com>
2018-11-07 19:10:38 +0400

    Added all fragments for home activity Trying to add materialUI

--- app/src/main/java/com/example/b00063271/safesplit/HomeScreenActivity.java
+++ app/src/main/java/com/example/b00063271/safesplit/HomeScreenActivity.java
@@ -4,25 +4,29 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class HomeScreenActivity extends AppCompatActivity implements FriendsFragment.OnFragmentInteractionListener {
+public class HomeScreenActivity extends AppCompatActivity implements FriendsFragment.OnFragmentInteractionListener,DashboardFragment.OnFragmentInteractionListener
+                ,ProfileFragment.OnFragmentInteractionListener,GroupsFragment.OnFragmentInteractionListener{
 
     private TextView mTextMessage;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
+    private final String TAG="HSActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
         fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
+        openFragment(new FriendsFragment());
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -35,31 +39,31 @@ public class HomeScreenActivity extends AppCompatActivity implements FriendsFrag
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.naviagation_friends:
-                    FriendsFragment fragment = new FriendsFragment();
-                    fragmentTransaction.add(R.id.frame_container, fragment);
-                    fragmentTransaction.commit();
-                    return true;
+                    return openFragment(new FriendsFragment());
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText("dashboard");
-                    return true;
+                    return openFragment(new DashboardFragment());
                 case R.id.naviagtion_me:
-                    mTextMessage.setText("me");
-                    return true;
+                    return openFragment(new ProfileFragment());
                 case R.id.navigation_groups:
-                    mTextMessage.setText("groups");
-                    return true;
+                    return openFragment(new GroupsFragment());
                 case R.id.navigation_new_bill:
-                    mTextMessage.setText("new");
+                    Log.d(TAG, "onNavigationItemSelected: Entering + activity");
                     return true;
             }
             return false;
         }
     };
-
+    private boolean openFragment(Fragment fragment){
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        return true;
+    }
 
 
     @Override
     public void onFragmentInteraction(Uri uri) {
-
+        Log.d(TAG, "onFragmentInteraction: "+uri);
     }
 }
